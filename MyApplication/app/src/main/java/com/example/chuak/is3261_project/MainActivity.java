afc5268c8251279efdf86e09837e65332c2ce348
SugarContent <chuakianchiu@gmail.com>
2017-09-19 23:00:38 +0800

    initial commit for android app

---
+++ MyApplication/app/src/main/java/com/example/chuak/is3261_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chuak.is3261_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
